d2882a9ba2b2db17b8ae170b309447436d8476a1
noiz354 <noiz354@gmail.com>
2017-10-16 15:37:31 +0700

    working getting image from camera.

--- app/src/main/java/com/stuart/hackatonproject/fragment/DetailReminderFragment.java
+++ app/src/main/java/com/stuart/hackatonproject/fragment/DetailReminderFragment.java
@@ -1,30 +1,63 @@
 package com.stuart.hackatonproject.fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.stuart.hackatonproject.R;
 import com.stuart.hackatonproject.model.ReminderDB;
 import com.stuart.hackatonproject.util.FirebaseUtils;
+import com.stuart.hackatonproject.util.GenericFileProvider;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by nathan on 10/11/17.
  */
-
+@RuntimePermissions
 public class DetailReminderFragment extends Fragment {
 
     public static final String EXTRA_REMINDER = "EXTRA_REMINDER";
+    private static final int CAMERA_REQUEST = 192;
+    private ImageView imageViewAttachment1, imageViewAttachment2;
+    private Uri imageToUploadUri;
+    private String authority;
+    File f;
 
     public static Fragment instance(Context context) {
         return new DetailReminderFragment();
@@ -50,6 +83,8 @@ public class DetailReminderFragment extends Fragment {
         titleTextView = view.findViewById(R.id.edit_text_title);
         contentTextView = view.findViewById(R.id.edit_text_content);
         reminderAtTextView = view.findViewById(R.id.edit_text_reminder_at_time);
+        setAuthority();
+        initImageUI(view);
         loadData();
         return view;
     }
@@ -91,5 +126,179 @@ public class DetailReminderFragment extends Fragment {
     private void sendTo(String toUserId) {
         reminderDB.setToUserId(toUserId);
         reminderDB.save();
+    }
+
+    private void initImageUI(View view){
+        imageViewAttachment1 = view.findViewById(R.id.image_view_image_attachment_1);
+        imageViewAttachment2= view.findViewById(R.id.image_view_image_attachment_2);
+        imageViewAttachment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetailReminderFragmentPermissionsDispatcher.readAndWriteStorageWithPermissionCheck(DetailReminderFragment.this);
+            }
+        });
+        imageViewAttachment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        DetailReminderFragmentPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @NeedsPermission({Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE})
+    void readAndWriteStorage(){
+        DetailReminderFragmentPermissionsDispatcher.showCameraWithPermissionCheck(DetailReminderFragment.this);
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    void showCamera() {
+
+        Intent chooserIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        chooserIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        f = new File(Environment.getExternalStorageDirectory(), "POST_IMAGE.jpg");
+        chooserIntent.putExtra(MediaStore.EXTRA_OUTPUT, GenericFileProvider.getUriForFile(getContext(), authority, f));
+        imageToUploadUri = GenericFileProvider.getUriForFile(getContext(), authority, f);
+        chooserIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        startActivityForResult(chooserIntent, CAMERA_REQUEST);
+//        getSupportFragmentManager().beginTransaction()
+//                .replace(R.id.sample_content_fragment, CameraPreviewFragment.newInstance())
+//                .addToBackStack("camera")
+//                .commitAllowingStateLoss();
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForCamera(final PermissionRequest request) {
+//        new AlertDialog.Builder(this)
+//                .setMessage(R.string.permission_camera_rationale)
+//                .setPositiveButton(R.string.button_allow, (dialog, button) -> request.proceed())
+//                .setNegativeButton(R.string.button_deny, (dialog, button) -> request.cancel())
+//                .show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void showDeniedForCamera() {
+//        Toast.makeText(this, R.string.permission_camera_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void showNeverAskForCamera() {
+//        Toast.makeText(this, R.string.permission_camera_neverask, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            if(imageToUploadUri != null){
+                Uri selectedImage = imageToUploadUri;
+                getActivity().getContentResolver().notifyChange(selectedImage, null);
+                Bitmap reducedSizeBitmap = getBitmap(f.getPath());
+                if(reducedSizeBitmap != null){
+                    imageViewAttachment1.setImageBitmap(reducedSizeBitmap);
+//                    ImgPhoto.setImageBitmap(reducedSizeBitmap);
+//                    Button uploadImageButton = (Button) findViewById(R.id.uploadUserImageButton);
+//                    uploadImageButton.setVisibility(View.VISIBLE);
+                }else{
+                    Toast.makeText(getActivity(),"Error while capturing Image",Toast.LENGTH_LONG).show();
+                }
+            }else{
+                Toast.makeText(getActivity(),"Error while capturing Image", Toast.LENGTH_LONG).show();
+            }
+//            Bitmap photo = (Bitmap) data.getExtras().get("data");
+////            imageView.setImageBitmap(photo);
+////            knop.setVisibility(Button.VISIBLE);
+//
+//
+//            // CALL THIS METHOD TO GET THE URI FROM THE BITMAP
+//            Uri tempUri = getImageUri(getActivity().getApplicationContext(), photo);
+//
+//            // CALL THIS METHOD TO GET THE ACTUAL PATH
+//            File finalFile = new File(getRealPathFromURI(tempUri));
+//
+////            System.out.println(mImageCaptureUri);
+        }
+    }
+
+    private Bitmap getBitmap(String path) {
+        Uri uri = GenericFileProvider.getUriForFile(getContext(), authority, new File(path));
+        InputStream in = null;
+        try {
+            final int IMAGE_MAX_SIZE = 1200000; // 1.2MP
+            in = getActivity().getContentResolver().openInputStream(uri);
+
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, null, o);
+            in.close();
+
+
+            int scale = 1;
+            while ((o.outWidth * o.outHeight) * (1 / Math.pow(scale, 2)) >
+                    IMAGE_MAX_SIZE) {
+                scale++;
+            }
+            Log.d("", "scale = " + scale + ", orig-width: " + o.outWidth + ", orig-height: " + o.outHeight);
+
+            Bitmap b = null;
+            in = getActivity().getContentResolver().openInputStream(uri);
+            if (scale > 1) {
+                scale--;
+                // scale to max possible inSampleSize that still yields an image
+                // larger than target
+                o = new BitmapFactory.Options();
+                o.inSampleSize = scale;
+                b = BitmapFactory.decodeStream(in, null, o);
+
+                // resize to desired dimensions
+                int height = b.getHeight();
+                int width = b.getWidth();
+                Log.d("", "1th scale operation dimenions - width: " + width + ", height: " + height);
+
+                double y = Math.sqrt(IMAGE_MAX_SIZE
+                        / (((double) width) / height));
+                double x = (y / height) * width;
+
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(b, (int) x,
+                        (int) y, true);
+                b.recycle();
+                b = scaledBitmap;
+
+                System.gc();
+            } else {
+                b = BitmapFactory.decodeStream(in);
+            }
+            in.close();
+
+            Log.d("", "bitmap size - width: " + b.getWidth() + ", height: " +
+                    b.getHeight());
+            return b;
+        } catch (IOException e) {
+            Log.e("", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    private void setAuthority() {
+        authority = getContext().getApplicationContext().getPackageName() + ".util.GenericFileProvider";
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
     }
 }
