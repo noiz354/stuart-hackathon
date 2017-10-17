e51f6c2705f57601229b66bcdbd9c2d0de101078
nathaniel <nathaniel.khuana@tokopedia.com>
2017-10-17 13:31:54 +0700

    Adding cardview on item list

--- app/src/main/java/com/stuart/hackatonproject/activity/HomeActivity.java
+++ app/src/main/java/com/stuart/hackatonproject/activity/HomeActivity.java
@@ -1,10 +1,7 @@
 package com.stuart.hackatonproject.activity;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -23,9 +20,6 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.stuart.hackatonproject.R;
 import com.stuart.hackatonproject.activity.base.BaseActivity;
 import com.stuart.hackatonproject.fragment.SharedReminderFragment;
