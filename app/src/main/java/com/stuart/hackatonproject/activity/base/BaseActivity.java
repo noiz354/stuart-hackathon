58b1f22f3625dec3727f02ffd3b8309320c053a1
hendry <hendry.setiadi.89@gmail.com>
2017-10-17 13:27:54 +0700

    fix setTheme

--- app/src/main/java/com/stuart/hackatonproject/activity/base/BaseActivity.java
+++ app/src/main/java/com/stuart/hackatonproject/activity/base/BaseActivity.java
@@ -37,11 +37,11 @@ public class BaseActivity extends AppCompatActivity {
     @CallSuper
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setUpAppTheme();
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setUpToolbar();
-        setUpAppTheme();
     }
 
     protected void setUpToolbar() {
